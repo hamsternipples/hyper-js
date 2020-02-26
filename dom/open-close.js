eb66bd02db70630749d6ce70aeb5794a7caf5ca8
flames of love <mechanicofthesequence@gmail.com>
2020-02-26 09:43:07 -0700

    roadability

--- dom/open-close.js
+++ dom/open-close.js
@@ -1,7 +1,5 @@
 import { extend, after } from '@hyper/utils'
 
-
-
 export default function open_close (opts = {}) {
   opts = extend({
     autoclose: 'autoclose',
@@ -13,16 +11,36 @@ export default function open_close (opts = {}) {
 
   return {
     open_briefly: function (sec, cb) {
-      return (!opts.pinned && this.classList.contains(opts.closed) ? (after(sec, () => {
-          if (this.classList.contains(opts.autoclose)) this.close()
-          cb && cb()
-        }), this.classList.add(opts.autoclose), this.open())
-      : false)
+      return (!opts.pinned && this.classList.contains(opts.closed)
+        ? (
+          after(sec, () => {
+            if (this.classList.contains(opts.autoclose)) this.close()
+            cb && cb()
+          }),
+          this.classList.add(opts.autoclose),
+          this.open()
+        )
+        : false
+      )
     },
-    toggle: function () { return this.classList.toggle(opts.closed), opts.is_open = this.classList.toggle(opts.open) },
-    open: function () { return this.classList.add(opts.open), this.classList.remove(opts.closed), opts.is_open = true },
-    close: function () { return this.classList.remove(opts.autoclose, opts.open), this.classList.add(opts.closed), opts.is_open = false },
-    pin: function () { return this.classList.remove(opts.autoclose), opts.pinned = !opts.pinned },
+    toggle: function () {
+      return this.classList.toggle(opts.closed),
+        opts.is_open = this.classList.toggle(opts.open)
+    },
+    open: function () {
+      return this.classList.add(opts.open),
+        this.classList.remove(opts.closed),
+        opts.is_open = true
+    },
+    close: function () {
+      return this.classList.remove(opts.autoclose, opts.open),
+        this.classList.add(opts.closed),
+        opts.is_open = false
+    },
+    pin: function () {
+      return this.classList.remove(opts.autoclose),
+        opts.pinned = !opts.pinned
+    },
     is_open: () => opts.is_open,
   }
 }
