ada187e4a4a38868d0a9d1b52afb5b3458fb8e88
flames of love <mechanicofthesequence@gmail.com>
2020-02-28 13:40:10 -0700

    random_date

--- random.js
+++ random.js
@@ -8,3 +8,10 @@ export const random_number2 = (max, min = 0) => min + (Math.sqrt(Math.random() *
 export const sample_array = (array, length) => {
   return (length || (length = array == null ? 0 : array.length)) ? array[random_idx(length)] : undefined
 }
+
+export const random_date = (days = 0, hours = Math.random() * 24, mins = Math.random() * 60, secs = Math.random() * 60) => Math.round(Date.now()
+    - (( (0.5 + Math.random()) * 1000 * 60 * 60 * 24 ) * days  )
+    - (( (0.5 + Math.random()) * 1000 * 60 * 60      ) * hours )
+    - (( (0.5 + Math.random()) * 1000 * 60           ) * mins  )
+    - (( (0.5 + Math.random()) * 1000                ) * secs  )
+  )
