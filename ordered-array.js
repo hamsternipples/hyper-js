2499b3f06f802100c7231e6d8faa4cdb0503be70
flames of love <mechanicofthesequence@gmail.com>
2020-03-21 22:20:43 -0600

    slight optimisations

--- ordered-array.js
+++ ordered-array.js
@@ -1,13 +1,26 @@
 
-export function insert (d, array, comparer) {
-  let pos = posOf(d, array, comparer) + 1
+
+export const idx_of = (d, array, comparer, start = 0, end = array.length) => {
+  if (end <= 0) return -1
+
+  var pivot = (start + end) >> 1
+  var c = comparer(d, array[pivot])
+
+  if (end - start <= 1) return c < 0 ? pivot - 1 : pivot
+  if (c === 0) return pivot
+  else if (c < 0) return idx_of(d, array, comparer, start, pivot)
+  else /* c > 0 */ return idx_of(d, array, comparer, pivot, end)
+}
+
+export const insert = (d, array, comparer) => {
+  let pos = idx_of(d, array, comparer) + 1
   if (pos === array.length) array.push(d)
   else array.splice(pos, 0, d)
   return pos
 }
 
-export function insert_d (d, array, exists, comparer) {
-  let pos = posOf(d, array, comparer) + 1
+export const insert_d = (d, array, exists, comparer) => {
+  let pos = idx_of(d, array, comparer) + 1
   if (pos === array.length) array.push(d)
   else array.splice(pos, 0, d)
   // update all exists values if they're greater than pos
@@ -17,8 +30,8 @@ export function insert_d (d, array, exists, comparer) {
   return pos
 }
 
-export function remove_d (id, array, exists, _pos) {
-  let pos = _pos === void 0 ? exists[id] : _pos
+export const remove_d = (id, array, exists, _pos) => {
+  let pos = _pos === undefined ? exists[id] : _pos
   if (pos !== void 0) {
     // possible optimization here, if pos == 0, then unshift, else if pos == array.length - 1 then pop, else:
     array.splice(pos, 1)
@@ -29,21 +42,4 @@ export function remove_d (id, array, exists, _pos) {
   }
 
   return pos
-}
-
-export function posOf (d, array, comparer, start, end) {
-  if (array.length === 0) return -1
-
-  start = start || 0
-  end = end || array.length
-  var pivot = (start + end) >> 1
-
-  var c = comparer(d, array[pivot])
-  if (end - start <= 1) return c === -1 ? pivot - 1 : pivot
-
-  switch (c) {
-    case -1: return posOf(d, array, comparer, start, pivot)
-    case 0: return pivot
-    case 1: return posOf(d, array, comparer, pivot, end)
-  }
 }
--- parse/videoUrls.js
+++ parse/videoUrls.js
@@ -2,43 +2,40 @@
 import Url from 'url'
 import qs from 'querystring'
 
-var vimeo_regex = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/
-export function vimeo_id (url) {
-  var match = url.match(vimeo_regex)
+const vimeo_regex = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/
+export const vimeo_id = (url, __match) => {
+  __match = url.match(vimeo_regex)
   return match && typeof match[3] === 'string' ? match[3] : null
 }
 
-var yt_regex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
-export function youtube_id (url) {
-  var match = url.match(yt_regex)
+const yt_regex = /^.*((youtu.be\/)|(__v\/)|(\/__u\/\w\/)|(embed\/)|(watch\?))\??__v?=?([^#\&\?]*).*/
+export const youtube_id = (url, __match) => {
+  __match = url.match(yt_regex)
   return match && match[7].length === 11 ? match[7] : null
 }
 
-var vine_regex = /^http(?:s?):\/\/(?:www\.)?vine\.co\/v\/([a-zA-Z0-9]{1,13}).*/
-export function vine_id (url) {
-  var match = url.match(vine_regex)
+const vine_regex = /^http(?:s?):\/\/(?:www\.)?vine\.co\/__v\/([a-zA-Z0-9]{1,13}).*/
+export const vine_id = (url, __match) => {
+  __match = url.match(vine_regex)
   return match && match[1].length === 11 ? match[1] : null
 }
 
 // to parse this:
 // <iframe src="http://cdnapi.kaltura.com/p/1910301/sp/191030100/embedIframeJs/uiconf_id/28928951/partner_id/1910301?iframeembed=true&playerId=verteletv-main-clip-571e3bcee87d1&entry_id=1_a9s8ncyo&flashvars[streamerType]=auto" width="560" height="395" allowfullscreen webkitallowfullscreen mozAllowFullScreen frameborder="0"></iframe>
 //                                                                                                                                                                 ^^^^^^^^^^^^^    |     ^^^^^^^^^^
-export function vertele_id (url) {
-  // if (!~url.indexOf('verteletv-main-clip')) return null
-  var u, v
-  if (u = Url.parse(url)) {
-    v = qs.parse(u.query)
-    u = v.playerId
+export const vertele_id = (url, __url, __qs) => {
+  if (__url = Url.parse(url)) {
+    __qs = qs.parse(__url.query)
+    __url = __qs.playerId
   }
-  return v && u ? u.substr(u.lastIndexOf('-') + 1) + '|' + v.entry_id : null
+  return __qs && __url ? __url.substr(__url.lastIndexOf('-') + 1) + '|' + __qs.entry_id : null
 }
 
-export function iframe_src (txt) {
-  var url, i
-  if (~(i = txt.indexOf('src="'))) {
-    url = txt.substring(i+5)
-    url = url.substr(0, url.indexOf('"'))
-    return url
+export const iframe_src = (txt, __url, __idx) => {
+  if (~(__idx = txt.indexOf('src="'))) {
+    __url = txt.substring(__idx+5)
+    __url = __url.substr(0, __url.indexOf('"'))
+    return __url
   }
-  return url ? url : txt
+  return __url ? __url : txt
 }
