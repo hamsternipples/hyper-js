754c1068efa39d499b5457ecb479ac3d14185bd2
flames of love <mechanicofthesequence@gmail.com>
2020-03-26 18:40:44 -0600

    positioned decorator

---
+++ decorators/positioned.js
@@ -0,0 +1,48 @@
+
+import { value } from '@hyper/dom/observable'
+import { getComputedStyle, bounding_rect } from '@hyper/dom-base'
+import { get_prop_value, int_prop_value, sum_prop_values } from '@hyper/dom-base'
+
+function positioned (node, target) {
+  if (ANCIENT) error('not supported yet. add: https://github.com/juggle/resize-observer')
+  var style = getComputedStyle(node)
+  var sizing = get_prop_value(style, 'box-sizing') === 'border-box' || (ANCIENT && (
+      get_prop_value(taStyle, '-moz-box-sizing') === 'border-box' ||
+      get_prop_value(taStyle, '-webkit-box-sizing') === 'border-box'))
+
+  if (node.w) debugger
+
+  node.w = value()
+  // node.w.v = () => node.offsetWidth
+  node.h = value()
+  // node.h.v = () => node.offsetHeight
+  node.x = value()
+  // node.x.v = () => node.clientOffsetX
+  node.y = value()
+  // node.y.v = () => node.clientOffsetY
+
+  if (TMP) {
+    node.w(v => {
+      console.log('setting width:', v)
+      if (!v) debugger
+    })
+  }
+
+  var setter = (rect) => {
+    console.log('setting rect:', rect)
+    if (rect.width === 0) debugger
+    node.w(rect.width)
+    node.h(rect.height)
+    node.x(rect.x)
+    node.y(rect.y)
+  }
+  var entry, obv = new ResizeObserver(entries => {
+    for (entry of entries) setter(entry.contentRect)
+    // for (entry of entries) setter('border-box' ? entry.borderBoxSize : entry.contentBoxSize)
+  }).observe(node, {box: sizing})
+
+  setter(bounding_rect(node))
+  return () => obv.disconnect()
+}
+
+export default positioned
