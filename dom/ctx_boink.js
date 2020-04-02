5bf54dfa4034710cb28f139a68e80dc662249909
flames of love <mechanicofthesequence@gmail.com>
2020-04-01 18:03:04 -0600

    ctx_boink working as intended

--- dom/ctx_boink.js
+++ dom/ctx_boink.js
@@ -4,36 +4,32 @@ import { ctx_el } from '@hyper/dom/hyper-hermes'
 import { remove_every, each } from '@hyper/array'
 import { boink } from '@hyper/dom/observable-event'
 
-// nocommit
-// - new function `ctx_el` - which returns the element for the ctx now G.E.frame
-// - rename to ctx_boink
-
 // the way in works is it'll call the callback, and if the callback's return is truthy, then the callback will be removed and won't be called again.
 
 var handlers = new WeakMap
 const ctx_boink = (G) => {
-  var handler, el
-  if (handler = handlers.get(G)) return handler
+  var handler = handlers.get(G)
+  if (handler) return handler
 
   var el = ctx_el(G)
   var click_handlers = []
 
   // context click handler
   boink(G.x, el, (ev) => {
-    var path = ev.path
+    var path = ev.path, i = 0
 
     each(click_handlers, ([not_on_this_el, cb], idx) => {
       if (!not_on_this_el || !path.includes(not_on_this_el)) {
-        if (!cb(ev)) click_handlers[idx] = null
+        if (!cb(ev)) click_handlers[idx] = null, i++
       }
     })
 
     // remove the nulls
-    remove_every(click_handlers)
+    if(i > 0) remove_every(click_handlers)
 
-    // tells it to continue to propagate the event.
+    // truthy value tells it to continue to propagate the event.
     // otherwise the boink function will stop the event's propagation.
-    return true
+    return 1
   }, true) // true = capture
 
   handler = (not_on_this_el, cb) => {
