c32c3684121a8862c65cedb9854c549c6df933d0
flames of love <mechanicofthesequence@gmail.com>
2020-05-06 01:46:40 -0600

    once

--- dom/observable.js
+++ dom/observable.js
@@ -43,7 +43,7 @@ export const bind2 = (l, r) => {
 // An observable that stores a value.
 if (DEBUG) var VALUE_LISTENERS = 0
 if (DEBUG) var OBV_ID = 0
-export const value = (initial, _obv) => {
+export const value = (initial) => {
   const obv = (val, do_immediately) => {
     return (
       is_void(val) ? obv.v  // getter
@@ -332,6 +332,55 @@ export const calc = (obvs, compute_fn) => {
   }
 
   return compute_fn.apply(null, obv_vals)
+}
+
+// the opposite of an obv, it just fires once then clears the listener
+export const once = (initial) => {
+  const obv = (val, do_immediately) => {
+    return (
+      is_void(val) ? obv.v  // getter
+    : not_fn(val) ? (       // is a set
+      is(obv.v, val) ? undefined : // only set if obv.v === val (but use Object.is instead of ===, so it gets NaN and +0/-0)
+        emit(obv.l,
+          obv.v, // previous value
+          obv.v = val  // new value
+        ),
+        obv.l = [], // reset the listeners array
+        val // return the value
+      )
+    : ( // is a listener function
+        obv.l.push(val),
+        (DEBUG && VALUE_LISTENERS++),
+        (
+          // if the value is uninitialised, or do_immediately is falsey, don't call he listener back immediately with the value.
+          is_void(obv.v) || is_falsy(do_immediately)
+          ? obv.v
+          : val(obv.v)
+        ), () => { // listener
+          remove(obv.l, val)
+          DEBUG && VALUE_LISTENERS--
+        }
+      )
+    )
+  }
+
+  if (is_obv(initial)) {
+    // cheap hack: the remove function is passed into our own liseners
+    //             so it'll remove itself when it fires.
+    // I set do_immediately to falsy value so it does't fire right away/
+    obv.l.push(initial(obv, 0))
+    // then, I steal it's current value
+    obv.v = initial.v
+  } else {
+    obv.v = initial
+  }
+
+  obv.l = []
+  obv._obv = 'value'
+  if (DEBUG) obv._id = OBV_ID++
+  if (DEBUG) obv._type = 'once'
+  if (DEBUG) obv.gc = () => compactor(obv.l)
+  return obv
 }
 
 export const boolean = (obv, truthy, falsey) => {
